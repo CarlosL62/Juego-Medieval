64481c64ff346dc1559586b518774190070307d6
CarlosL62 <carlosalfredolopez96@gmail.com>
2023-03-29 01:15:36 -0600

    Actualizacion final

--- JuegoGamePlay/src/main/java/com/mycompany/juegogameplay/JuegoGamePlay.java
+++ JuegoGamePlay/src/main/java/com/mycompany/juegogameplay/JuegoGamePlay.java
@@ -8,12 +8,15 @@ import com.mycompany.juegogameplay.Menu.Menu;
  * @author carlos
  */
 public class JuegoGamePlay {
+
     
     public static void main(String[] args) {
+
         Consola.LimpiarPantalla();
         Consola.Titulo("La aventura del aventurero");
         Consola.EnterContinuar();
         Menu menu = new Menu();
         menu.mostrarMenu();
+
     }
 }
